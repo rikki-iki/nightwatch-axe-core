c75bd9bc3b79143b467c2af41167fa21cac35587
rikki_iki <rikki.bochow@gmail.com>
2020-09-24 17:38:06 +1000

    Add commands and readme

---
+++ README.md
@@ -0,0 +1,124 @@
+# Nightwatch aXe-core
+
+Nightwatch.js commands for running aXe-core.
+
+## Installation
+
+Install using yarn or npm
+
+```bash
+npm install nightwatch-axe-core --save-dev
+```
+
+Add these commands to the `custom_commands_path` in your Nightwatch.js configuration.
+
+```js
+{
+  custom_commands_path : [
+    "./node_modules/nightwatch-axe-core/commands"
+  ]
+}
+```
+
+## Configuration & Usage
+
+The `axe()` command takes the following two parameters:
+
+Parameter Name | Parameter Type   | Description
+-------------  | ---------------- | -----------
+context        | string or object | css selector or [include/exclude object](https://github.com/dequelabs/axe-core/blob/master/doc/API.md#context-parameter)
+options        | object           | set of [axe options](https://github.com/dequelabs/axe-core/blob/master/doc/API.md#options-parameter)
+
+These can be defined globally and/or per call to the custom `axe()` command.
+
+__In addition to the standard aXe `options`:__
+
+- `options.verbose` set to `true` will log all successful `aXe` tests.
+- `options.timeout` configures Nightwatch's `timeoutsAsyncScript()` amount, default value is `1000 milliseconds`.
+
+aXe can require a fair amount of time to run, so increasing the `timeout` option is often required.
+
+__Injecting aXe-core__
+
+The `axe()` command will inject `./node_modules/axe-core/axe-core.min.js` into your test fixture, just once, by running 
+the included `injextAxe()` command and checking for it at the start of the `axe()` command.
+
+If it fails to inject or find the script it will throw an error. If it fails please check the `axe-core` package has 
+been installed.
+
+### Global configuration file
+
+Create an `axe.conf.js` file in your project root as an CommonJS module that exports a default object with both the 
+`context` and `options` parameters:
+
+```js
+// axe.conf.js
+
+module.exports = {
+  context: {
+    include: [['html']],
+    exclude: [['.advertising']],
+  },
+  options: {
+    runOnly: {
+      type: 'tag',
+      values: ['wcag2a', 'wcag2aa'],
+    },
+    verbose: true,
+    timeout: 2000,
+  }
+};
+```
+
+Then your test simply needs to call the `axe()` command.
+
+```js
+// nightwatch-test.js
+
+export default {
+  '@tags': ['accessibility'],
+
+  'Thing passes aXe-core checks': function (browser) {
+    browser
+      .url(`${browser.launch_url}/page-to-test`)
+      .waitForElementPresent('.thing-to-test')
+      .axe()
+      .end()
+  }
+}
+```
+
+### Per test configuration
+
+When calling `axe()` you can can pass in the `context` and `options` values as arguments to override any global 
+configuration. 
+
+```js
+axe(context, options)
+```
+
+For example;
+```js
+// nightwatch-test.js
+
+export default {
+  '@tags': ['accessibility'],
+
+  'Thing passes aXe-core checks': function (browser) {
+    browser
+      .url(`${browser.launch_url}/page-to-test`)
+      .waitForElementPresent('.thing-to-test')
+      .axe('.thing-to-test', {
+        runOnly: {
+          type: 'tag',
+          values: ['wcag2a']
+        },
+        rules: {
+          'color-contrast': { enabled: true },
+          'valid-lang': { enabled: false }
+        }
+      })
+      .end()
+  }
+}
+```
---
+++ commands/axe.js
@@ -0,0 +1,120 @@
+/* eslint-disable func-names, max-nested-callbacks, no-console */
+/**
+ * @file Custom axe-core runner.
+ */
+
+const path = require('path');
+const fs = require('fs');
+const CONFIG_FILENAME = 'axe.con.js';
+
+/**
+ * Check and run Axe.
+ * See https://github.com/dequelabs/axe-core/blob/develop/doc/API.md#api-name-axerun
+ *
+ * @param {string} cxt - The html selector to run on.
+ * @param {object} opt - Axe options.
+ * @param {function} done - Done callback.
+ */
+const runAxe = (cxt, opt, done) => {
+  if (!window.axe) {
+    done({ error: 'window.axe not found. Try increasing the "options.timeout" in your axe.conf file.' });
+  }
+
+  window.axe
+    .run(cxt, opt)
+    .then((results) => {
+      done({ results });
+    })
+    .catch((error) => {
+      done({ error: error.toString() });
+    });
+};
+
+/**
+ * Nightwatch command to load and run Axe.
+ *
+ * @param {string} customContext - The html selector to runAxe() on. Defaults to axe.conf context.
+ * @param {object} customOptions - Axe options. Defaults to axe.conf options.
+ * @return {command} - Return this to make sure commands can be chained.
+ */
+module.exports.command = function (customContext, customOptions) {
+  let defaultConfig = {
+    options: {
+      timeout: 1000,
+    },
+    context: 'html',
+  };
+  if (fs.existsSync(path.resolve(CONFIG_FILENAME))) {
+    defaultConfig = require(path.resolve(CONFIG_FILENAME));
+  }
+
+  const { context, options } = defaultConfig;
+  const ctx = customContext || context;
+  const opt = customOptions || options;
+
+  this.perform(() => {
+    // Set Nightwatch async script timeout option,
+    // because axe-core can need more time.
+    this.timeoutsAsyncScript(opt.timeout || 1000);
+
+    // Make sure axe-core script is included.
+    // See /tests/nightwatch/commands/includeAxe.js
+    this.includeAxe((axe) => {
+      if (!axe) {
+        throw new Error('The includeAxe() command failed to append the axe-core script to the fixture. See /tests/nightwatch/commands/includeAxe.js');
+      }
+
+      // Execute runeAxe() asynchronously.
+      this.executeAsync(runAxe, [ctx, opt], (response) => {
+        const { results } = response.value;
+        if (response.errorStatus === 28) {
+          throw new Error('Try increasing the "options.timeout" in your axe.conf.js file.');
+        }
+        else if (response.error) {
+          throw new Error(response.error);
+        }
+
+        const { passes, violations } = results;
+        const passCount = passes.length;
+        const failCount = violations.length;
+
+        if (opt.verbose) {
+          // If verbose list each pass as well.
+          passes.forEach((pass) => {
+            this.assert.ok(true, pass.help);
+          });
+        }
+
+        if (passCount > 0 && failCount === 0) {
+          // Show passed count.
+          this.assert.ok(true, `Axe: ${passCount} passed.`);
+        }
+
+        violations.forEach((violation, i) => {
+          i += 1;
+          // If violations, list each one with as much information as possible.
+          const {
+            id, help, helpUrl, impact, nodes,
+          } = violation;
+          const url = helpUrl.split('?')[0];
+
+          // Console log for better formatting.
+          console.log(`-----aXe-----\n#${i} - ${help} (${id})\nImpact: ${impact}\nCount: ${nodes.length}\nSee: ${url}`);
+          nodes.forEach((node, ii) => {
+            ii += 1;
+            const { failureSummary, html } = node;
+            console.log(`#${i}.${ii} - ${failureSummary}\n  ${html}`);
+          });
+          console.log('-------------');
+        });
+
+        if (failCount > 0) {
+          // Show violation count and fail.
+          this.assert.fail(`Axe: ${failCount} rule violation(s). See console output for details`);
+        }
+      });
+    });
+  });
+
+  return this;
+};
